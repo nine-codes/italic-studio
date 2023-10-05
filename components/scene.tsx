04738185c058a5ecb3fa91fba5f7f85be98554f2
Nine <abdelrhmanshalil@gmail.com>
2023-10-05 16:48:56 +0200

    rotate model

--- components/scene.tsx
+++ components/scene.tsx
@@ -4,7 +4,7 @@ import { Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
 
 import Model from './model';
-import { Stage } from '@react-three/drei';
+import { OrbitControls, Stage } from '@react-three/drei';
 
 export default function Scene() {
   return (
@@ -12,6 +12,7 @@ export default function Scene() {
       className='w-full h-full !fixed bg-transparent'
       camera={{ fov: 35, zoom: 1.3, near: 1, far: 1000 }}
     >
+      <OrbitControls autoRotate={true} enableZoom={false} />
       <ambientLight intensity={0.3} />
       <directionalLight color='#FFFFFF' position={[10, 10, 5]} intensity={1} />
       <directionalLight color='#FFFFFF' position={[0, 10, 0]} intensity={1.5} />

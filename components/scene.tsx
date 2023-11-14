88b5fecb4a3ea5d93f54f016b79c2447a370026d
Nine <abdelrhmanshalil@gmail.com>
2023-11-14 16:40:18 +0100

    use iframe

--- components/scene.tsx
+++ components/scene.tsx
@@ -1,11 +1,10 @@
-'use client';
-import Spline from '@splinetool/react-spline';
-
 export default function Scene() {
   return (
-    <Spline
-      className='w-full h-full !fixed bg-transparent'
-      scene='https://prod.spline.design/g5knsLX7fplprEVT/scene.splinecode'
-    />
+    <iframe
+      className='w-full h-full fixed bg-transparent'
+      src='https://my.spline.design/cubic-3430883512c72f08cb78dadf0e6456ad/'
+      width='100%'
+      height='100%'
+    ></iframe>
   );
 }
